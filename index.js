55481e0ee2b299520711813df894267f2c56fe4f
Nguyen <nguyentkhd2023@gmail.com>
2023-10-26 22:43:19 +0700

    Them source

---
+++ index.js
@@ -0,0 +1,5 @@
+// ------------------------------Slider-------------------------------
+const rightarrow = document.querySelector('.fa-chevron-right')
+rightarrow.addEventListener ("click", function(){
+    document.querySelector("slider-content-left-top").style.
+})
